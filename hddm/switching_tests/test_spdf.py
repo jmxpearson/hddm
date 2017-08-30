8897298b4f4f22f0b3b58d7af1042e11ca137736
Pritha Ghoshal <ghoshal.pritha@gmail.com>
2017-08-30 18:50:13 -0400

    Adding more tests for formula B

--- hddm/switching_tests/test_spdf.py
+++ hddm/switching_tests/test_spdf.py
@@ -8,6 +8,13 @@ class TestABParams(unittest.TestCase):
     def runTest(self):
         pass
 
+    def getExpectedValueB(self, j, k, dv, sign):
+        temp = 1.0 * (-1) ** (sign + 1)
+        B = temp * 4.0 * np.pi**2 * j * k * dv * (((-1) ** (j + k) * np.exp(temp*dv) - 1)) / (2.0 * np.pi**2 * (j**2 + k**2) * dv**2 + 1.0*np.pi**4 * (j**2 - k**2)**2 + dv**4)
+        return B
+
+
+
     def test_check_B(self, size=100):
         j = 1
         k = 0
@@ -29,4 +36,16 @@ class TestABParams(unittest.TestCase):
         sign = 0
         expected = 0.61650432
         m = swfpt.BB(k, j, dv, sign)
+        B = self.getExpectedValueB(k, j, dv, sign)
         np.testing.assert_almost_equal(m, expected)
+        np.testing.assert_almost_equal(B, expected)
+        np.testing.assert_almost_equal(m, B)
+
+        for j in range(1, 5):
+            for k in range(1, 5):
+                for dv in np.linspace(0.0, 3.0, num=7):
+                    for sign in range(0, 2):
+                        expValue = self.getExpectedValueB(k, j, dv, sign)
+                        m = swfpt.BB(k, j, dv, sign)
+                        print ("k= ", k, " j = ", j, " dv = ", dv, " sign = ", sign)
+                        np.testing.assert_almost_equal(m, expValue)
