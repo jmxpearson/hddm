1eb046bb3f0d58d689358f75cfaab87cb9b9ce6f
Pritha Ghoshal <ghoshal.pritha@gmail.com>
2017-09-03 16:39:25 -0400

    Initial partial addition of pdf_kernel function in test script

--- hddm/switching_tests/test_spdf.py
+++ hddm/switching_tests/test_spdf.py
@@ -93,8 +93,85 @@ class TestABParams(unittest.TestCase):
         A_k_0_cy = vfunc(kx, vx, yx)
         np.testing.assert_almost_equal(A_k_0_cy, A_k_0)
 
+    # tt - time for which pdf is wanted
+    # w - z/a normalized
+    # v1 - diffusion rate 1
+    # v2 - diffusion rate 2
+    # s - Switching times
+    # n - number of elements in s before time t
+
+    def calculate_Ak(self, tt, w, v1, v2, s, n, err):
+        K = 2
+        A = np.array(np.zeros(K), dtype=np.double)
+        tmp = np.array(np.zeros(K), dtype=np.double)
+        k_val = np.arange(1, K+1)
+        tmp = np.zeros_like(tmp)
+        if (s[0] != 0):
+           s = np.insert(s, 0, 0)
+        diff = np.ediff1d(s)
+
+        #get dv
+        dv = v1 - v2
+        #starting value of A
+        A = self.getExpectedValueAk0(k_val, v1, w)
+        print ("A:", A)
+
+        #Calculate B matrix to be used
+        nj, nk, nsign = (2, 2, 2)
+        k = np.arange(1, nk+1)
+        j = np.arange(1, nj+1)
+        sign = np.arange(0, nsign)
+        xk, xj, xsign = np.meshgrid(k, j, sign)
+        B = self.getExpectedValueB(xk, xj, dv, xsign)
+        print ("B:", B)
+
+        #calculate lambda in array format
+        lambda_arr = (k**2) * (np.pi**2) / 2
+
+        #Array with one row for each lambda, one column for each time itertion tau
+        mul_factor = np.exp(-np.outer(lambda_arr, diff))
+
+
+        #Start with sign 0 and flip back and forth in every iteration
+        sign = 0
+        #going from 1 to n, calculate A-s iteratively
+        for i in range(n):
+            tau = diff[i-1] #difference from previous switching value
+            A = A * mul_factor[:, i-1]
+            #Multiply with B matrix for particular sign
+            A = np.dot(B[:,:,sign], A)
+            print ("iter:", i)
+            print (A)
+            sign = 1-sign
+
+
+
+
+
+    def test_check_pdf_kernel(self, size=100):
+
+        x = np.linspace(0, 10, 1000)
+        x_size = x.shape[0]
+        v1 = 2
+        v2 = 1
+        sv = 0
+        a = 1
+        z = 0.5
+        sz = 0
+        s = np.arange(0, 2, 0.1)
+        t = 0
+        st = 0
+        err = 1e-4
+        logp = 0
+        s_size = s.shape[0]
+        swfpt.pdf_array(x, v1, v2, sv, a, z, sz, s, t, st, err, logp)
+        self.calculate_Ak(x, z, v1, v2, s, s_size, err)
+
+
+
 if __name__ == "__main__":
     test = TestABParams()
     test.test_check_B()
     test.test_check_Ak0()
+    test.test_check_pdf_kernel()
 
